b349e8dfa4d1df228d4870093dd364c44556568b
orsimalaka <162946593+orsimalaka@users.noreply.github.com>
2024-03-29 13:06:30 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,6 +33,50 @@ export default function RootLayout({
 }: {
     children: React.ReactNode;
 }) {
+    const jsonLd = {
+        "@context": "https://schema.org",
+        "@type": "Organization",
+        "name": `${SITENAME}`,
+        "tagline": `${SITENAME} - Video Bokep Indo Jepang Barat Viral Terbaru`,
+        "description": "Bokep Tante Video bokep indo jepang indonesia barat terbaru viral korea simontok china tante live sma paksa ngentot abg jilbab cewek bocil smp pijat pelajar.",
+        "logo": "https://bokep-tante.pages.dev/favicon.ico",
+        "url": "https://bokep-tante.pages.dev",
+        "founder":{
+            "@type":"Person",
+            "name":"admin",
+            "url":"https://bokep-tante.pages.dev"
+            },
+            "foundingDate":"2024-02-01"
+        }
+        const jsonLd1 = {
+            "@context": "https://schema.org",
+        "@type": "WebPage",
+        "name": `${SITENAME}`,
+        "id": "https://bokep-tante.pages.dev/#WebPage",
+        "url": "https://bokep-tante.pages.dev",
+        "tagline": `${SITENAME} - Video Bokep Indo Jepang Barat Viral Terbaru`,
+        "description": "Bokep Tante Video bokep indo jepang indonesia barat terbaru viral korea simontok china tante live sma paksa ngentot abg jilbab cewek bocil smp pijat pelajar.",
+        "image": "https://bokep-tante.pages.dev/favicon.ico",
+        "potentialAction":{
+            "@type":"ReadAction",
+            "target":"https://bokep-tante.pages.dev/"}
+        }
+        const jsonLd2 = {
+            "@context": "https://schema.org",
+        "@type": "Website",
+        "name": `${SITENAME}`,
+        "id": "https://bokep-tante.pages.dev/#Website",
+        "url": "https://bokep-tante.pages.dev",
+        "tagline": `${SITENAME} - Video Bokep Indo Jepang Barat Viral Terbaru`,
+        "description": "Bokep Tante Video bokep indo jepang indonesia barat terbaru viral korea simontok china tante live sma paksa ngentot abg jilbab cewek bocil smp pijat pelajar.",
+        "potentialAction":{
+            "@type":"SearchAction",
+            "target":{
+                "@type":"EntryPoint",
+                "urlTemplate":"https://bokep-tante.pages.dev/?q={search_term_string}"
+            },
+            "query-input":"required name=search_term_string"}
+            }
     return (
         <html lang="en" suppressHydrationWarning>
             <body className={font.className}>
@@ -42,6 +86,18 @@ export default function RootLayout({
                     enableSystem
                     disableTransitionOnChange
                 >
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+        />
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd1) }}
+        />
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd2) }}
+        />
                     {children}
                 </ThemeProvider>
             </body>
