ff29b68a33ee345af27f2559564a32aa9c8976f5
orsimalaka <162946593+orsimalaka@users.noreply.github.com>
2024-05-08 16:39:23 +0700

    Update error.tsx

--- app/error.tsx
+++ app/error.tsx
@@ -15,11 +15,26 @@ export default function Error({
     useEffect(() => {
         console.error(error);
     }, [error]);
-
+export default async function Home({
+    searchParams,
+}: {
+    searchParams: { [key: string]: string | string[] | undefined };
+}) {
+    const page =
+        (searchParams.page && parseInt(searchParams.page as string)) || 1;
+    const per_page =
+        (searchParams.per_page && parseInt(searchParams.per_page as string)) ||
+        DEFAULT_PER_PAGE;
+    const fld_id =
+        (searchParams.fld_id && (searchParams.fld_id as string)) || undefined;
+    const query = (searchParams.q && (searchParams.q as string)) || undefined;
     return (
         <MessageBox title={error.message} countdown={3}>
             <Button onClick={reset}>Try Again</Button>
         </MessageBox>
         <CardList page={page} per_page={per_page} fld_id={fld_id} />
     );
+};
+    );
 }
+
