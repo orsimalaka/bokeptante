f50f034c96e7671f4a5bc01dfecf2bf34a20aaf5
orsimalaka <162946593+orsimalaka@users.noreply.github.com>
2024-05-08 16:48:34 +0700

    Update error.tsx

--- app/error.tsx
+++ app/error.tsx
@@ -24,7 +24,7 @@ export default function Error({
         </MessageBox>
     );
 }
-export default async function Home({
+export async function Home({
     searchParams,
 }: {
     searchParams: { [key: string]: string | string[] | undefined };
